aba825210eae7b9a078987d6717f75cf58598540
Lennart Van Hirtum <lennart.vanhirtum@gmail.com>
2026-04-06 02:36:28 +0200

    Simplify structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/src/extension.ts', // Path to your extension's entry point
+  entry: './src/extension.ts', // Path to your extension's entry point
   target: 'node', // Specify the environment (node or web)
   output: {
     filename: 'extension.js', // Output filename
